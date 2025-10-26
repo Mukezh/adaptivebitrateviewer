5a007a623984a2f6c28d58dcfdd5bf6dfe6134ad
Mukezh <mukesh.offcl@gmail.com>
2025-10-26 15:11:34 +0530

    oct 26

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,24 +1,30 @@
+import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const Card = () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const [title, setTitle] = useState("");
 
   return (
-    <>
-      <form className="h-128 w-lg py-2 px-4 border rounded-lg flex flex-col bg-[#F8E9B9]">
-        <div className="flex justify-center pb-4 text-xl font-family-justme">
-          <label>Upload videos</label>
+    <div className="flex items-center justify-center h-screen w-screen bg-[#F2EFCE]">
+      <form className="h-128 w-lg py-2 px-4 border rounded-lg flex flex-col gap-4 items-center justify-center bg-[#F8E9B9]">
+        <label className="text-3xl font-serif">Upload videos</label>
+        <div className="flex justify-center pb-4 text-xl font-family-justme"></div>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            className="border p-2 rounded-lg "
+            placeholder="Enter title for your video"
+            value={title}
+          />
         </div>
-        <label> Video title </label>
-        <input type="text" className="border p-2 rounded-lg w-64" />
-        <label>Upload Content</label>
-        <div className="pb-4 border rounded-lg w-64" {...getRootProps()}>
-          <p>Drag 'n' drop some files here</p>
+        <div className="p-4 border rounded-lg " {...getRootProps()}>
           <button className="w-64">
-            <input {...getInputProps()} /> click here to upload
+            <input {...getInputProps()} /> Click here to upload files
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
