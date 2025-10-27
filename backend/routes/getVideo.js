f062e843e22109e93587c8457e44673bc8eb6c04
Mukezh <mukesh.offcl@gmail.com>
2025-10-27 17:44:08 +0530

    oct 27

--- backend/routes/getVideo.js
+++ backend/routes/getVideo.js
@@ -1,31 +1,58 @@
-import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  ListObjectsV2Command,
+  paginateListObjectsV2,
+} from "@aws-sdk/client-s3";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const getVideoRouter = express.Router();
 const client = new S3Client({ region: process.env.AWS_REGION });
-const command = new ListObjectsV2Command({
-  Bucket: process.env.S3_BUCKET_NAME,
-  Prefix: "raw-videos/",
-});
+// const command = new paginateListObjectsV2({
+//   Bucket: process.env.S3_BUCKET_NAME,
+//   Prefix: "raw-videos/",
+// });
 
 getVideoRouter.get("/getVideo", async (req, res) => {
+  //   try {
+  //     const response = await client.send(command);
+  //     response.Contents.forEach((obj) => {
+  //       console.log(obj.Key);
+  //       res.json({ success: true, video: obj.Key });
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(500).json({ success: false, message: "video retrieval failed" });
+  //   }
+  // });
+
+  // const response = await client.send(command);
+  // response.Contents.forEach((obj) => {
+  //   console.log(obj.Key);
+  // });
+
   try {
-    const response = await client.send(command);
-    response.Contents.forEach((obj) => {
-      console.log(obj.Key);
-      res.json({ success: true, video: obj.Key });
-    });
+    const paginator = paginateListObjectsV2(
+      {
+        client: client,
+        pageSize: 10,
+      },
+      { Bucket: process.env.S3_BUCKET_NAME, Prefix: "raw-videos/" },
+    );
+    const allKeys = [];
+
+    for await (const page of paginator) {
+      // page.Contents is an array of { Key, Size, LastModified, ... }
+      const keysInPage = page.Contents?.map((obj) => obj.Key) ?? [];
+      allKeys.push(...keysInPage);
+    }
+
+    res.json({ status: 200, allKeys });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, message: "video retrieval failed" });
   }
 });
 
-const response = await client.send(command);
-response.Contents.forEach((obj) => {
-  console.log(obj.Key);
-});
-
 export default getVideoRouter;
+//
--- frontend/src/pages/Viewpage.jsx
+++ frontend/src/pages/Viewpage.jsx
@@ -1,17 +1,18 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// export const ViewPage = () => {
-//   const [data, setData] = useState([]);
+export const ViewPage = () => {
+  const [data, setData] = useState([]);
 
-//   useEffect(() => {
-//     const fetchdata = async () => {
-//       const response = await fetch("http://localhost:5000/getVideo");
-//       const result = response.json();
-//       setData(Array.isArray(result) ? result.video : [result.video]);
-//     };
-//     fetchdata();
-//   }, []);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("http://localhost:5000/getVideo");
+      const result = await response.json();
+      console.log(Object(result));
+      setData(Array.isArray(result) ? result.allKeys : [result.allKeys]);
+    };
+    fetchdata();
+  }, []);
 
-//   console.log(data);
-//   return <div>{data}</div>;
-// };
+  console.log(data);
+  return <div>{data[0]}</div>;
+};
